d561adb5fc8b111fdc9f88bb16af8a1f0f0441ff
doemu0992 <104934446+doemu0992@users.noreply.github.com>
2026-06-01 07:56:04 +0200

    Initial Commit

---
+++ PainDiary/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PainDiary
+//
+//  Created by Dominik Gerber on 01.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PainDiaryTests/PainDiaryTests.swift
@@ -0,0 +1,18 @@
+//
+//  PainDiaryTests.swift
+//  PainDiaryTests
+//
+//  Created by Dominik Gerber on 01.06.2026.
+//
+
+import Testing
+
+struct PainDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
